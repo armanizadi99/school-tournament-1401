4f2b18a2515293f448391b5e84c6b47f70e2cc20
ali <unixnexo@gmail.com>
2022-04-03 23:38:35 +0430

    adding some details also adding two php pages

---
+++ login.php
@@ -0,0 +1,7 @@
+<?php
+
+$login_username = $_POST["login_username"];
+$login_password = $_POST["login_password"];
+echo($login_username ." | ". $login_password);
+
+?>
---
+++ register.php
@@ -0,0 +1,8 @@
+<?php
+
+$singup_email = $_POST["singup_email"];
+$singup_username = $_POST["singup_username"];
+$singup_password = $_POST["singup_password"];
+echo($singup_email ." | ". $singup_username ." | ". $singup_password);
+
+?>
